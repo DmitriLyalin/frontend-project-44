55e96b920201d9e9aec2aed6cec04401e5098352
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-06 12:28:51 +0300

    fix return userName

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 let userName = ""
 const greetUser = ()=>{
+console.log('Welcome to the Barin Games!')
   userName = readlineSync.question('May I have your name? ');
 console.log('Hello ' + userName + '!');
+return userName
 
 }
-export {greetUser, userName} 
+export {greetUser} 
