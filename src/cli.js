d62470b2e7fdd4433d1ba2e068ed17447f9f40d2
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-07 20:22:37 +0300

    use prettier

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
-import readlineSync from 'readline-sync';
-let userName = ""
-const greetUser = ()=>{
-console.log('Welcome to the Barin Games!')
-  userName = readlineSync.question('May I have your name? ');
-console.log('Hello ' + userName + '!');
-return userName
-
-}
-export {greetUser} 
+import readlineSync from "readline-sync";
+let userName = "";
+const greetUser = () => {
+  console.log("Welcome to the Barin Games!");
+  userName = readlineSync.question("May I have your name? ");
+  console.log("Hello " + userName + "!");
+  return userName;
+};
+export { greetUser };
