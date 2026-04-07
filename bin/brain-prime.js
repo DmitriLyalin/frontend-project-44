13909fd91890074876b80434efd1c0a5a255b174
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-07 20:20:41 +0300

    use prettier

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -10,26 +10,24 @@ import {
 
 const range = 100;
 const isPrime = (number) => {
-let prime = 0
+  let prime = 0;
 
   if (number === 2 || number === 3) {
     return "yes";
   }
 
-if (number < 2 || number % 3 === 0) {
+  if (number < 2 || number % 3 === 0) {
     return "no";
   }
 
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
-     prime +=1
-    } 
-      
+      prime += 1;
+    }
   }
-    return prime === 0 ?"yes" : "no"
-
+  return prime === 0 ? "yes" : "no";
 };
-isPrime(59)
+isPrime(59);
 const app = () => {
   const userName = greetUser();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
@@ -44,7 +42,7 @@ const app = () => {
       console.log("Correct!");
     } else {
       console.log(
-        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`
+        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`,
       );
       return;
     }
@@ -52,4 +50,3 @@ const app = () => {
   console.log(`Congratulations, ${userName}!`);
 };
 app();
-
