98196a7aeb9c9223c4164c8de640477bbbabc8d0
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-08 12:43:50 +0300

    fix worng letter

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from "readline-sync";
 let userName = "";
 const greetUser = () => {
-  console.log("Welcome to the Barin Games!");
+  console.log("Welcome to the Brain Games!");
   userName = readlineSync.question("May I have your name? ");
   console.log("Hello " + userName + "!");
   return userName;
