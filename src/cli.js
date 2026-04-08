4e368863d7271abf3c0a1698b5deb4485762aa67
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-08 13:26:24 +0300

    fix comma

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ let userName = "";
 const greetUser = () => {
   console.log("Welcome to the Brain Games!");
   userName = readlineSync.question("May I have your name? ");
-  console.log("Hello " + userName + "!");
+  console.log("Hello, " + userName + "!");
   return userName;
 };
 export { greetUser };
