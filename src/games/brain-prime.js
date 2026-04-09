b8117f1df517525d9c3bde7adcdd59252ca3c73e
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-09 15:07:24 +0300

    fix: optimize prime number check

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,47 +6,36 @@ import {
   checkAnswer,
   getRandomNumber,
   gameRounds,
-} from '../index.js'
-
-const range = 100
+  getResult,
+ range,
+} from '../utils.js'
+ // Функция проверки: является ли число простым
 const isPrime = (number) => {
-  let prime = 0
-
-  if (number === 2 || number === 3) {
-    return 'yes'
-  }
-
-  if (number < 2 || number % 3 === 0) {
+  if (number < 2) {
     return 'no'
   }
-
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
-    if (number % i === 0) {
-      prime += 1
-    }
+     if (number % i === 0){
+            return 'no' 
   }
-  return prime === 0 ? 'yes' : 'no'
 }
+    return 'yes'
+}
+// Основная функция игры "Простое число"
 const brainPrime = () => {
   const userName = greetUser()
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
   for (let round = 0; round < gameRounds; round += 1) {
-    const questionNumber = getRandomNumber(range)
+   const questionNumber = getRandomNumber(range)
     askQuestion(questionNumber)
     const correctAnswer = isPrime(questionNumber)
     const userAnswer = getUserAnswer()
-
     const checkedAnswer = checkAnswer(userAnswer, correctAnswer)
-    if (checkedAnswer) {
-      console.log('Correct!')
-    }
-    else {
-      console.log(
-        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`,
-      )
-      return
-    }
+    const result =  getResult(checkedAnswer,userName,userAnswer,correctAnswer)  // Получаем результат (true/false)
+    if (!result) {
+	return}
   }
+  
   console.log(`Congratulations, ${userName}!`)
 }
 export { brainPrime }
