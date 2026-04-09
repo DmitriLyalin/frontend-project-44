bc118d38588d5f59b42d8c1d559e9a466f12a782
Dmitry Lyalin <lialin.dmitry@gmail.com>
2026-04-09 15:05:13 +0300

    refactor: repeated function into module

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,39 +1,33 @@
 #!/usr/bin/env node
 import { greetUser } from '../cli.js'
-import { askQuestion, getUserAnswer, checkAnswer } from '../index.js'
-
+import { askQuestion, getUserAnswer, checkAnswer, getResult, gameRounds, range } from '../utils.js'
+// Генерация случайного числа
 const getRandomNumber = (range) => {
   let randomNumber = Math.floor(Math.random() * range)
   return randomNumber
 }
-
+// Проверка на чётность
 const isEven = (randomNumber) => {
   return randomNumber % 2 === 0 ? 'yes' : 'no'
 }
 
-const range = 100
-const gameRounds = 3
 
+// Основная функция игры "Проверка на чётность"
 const brainEven = () => {
   const userName = greetUser()
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
+ // Запускаем цикл игры на заданное количество раундов
   for (let round = 0; round < gameRounds; round += 1) {
-    const questionNumber = getRandomNumber(range)
-    askQuestion(questionNumber)
-    const correctAnswer = isEven(questionNumber)
-    const userAnswer = getUserAnswer()
-
-    const checkedAnswer = checkAnswer(userAnswer, correctAnswer)
-    if (checkedAnswer) {
-      console.log('Correct!')
-    }
-    else {
-      console.log(
-        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. Let's try again, ${userName}!`,
-      )
-      return
-    }
+    const questionNumber = getRandomNumber(range)  
+    askQuestion(questionNumber) /
+    const correctAnswer = isEven(questionNumber) 
+    const userAnswer = getUserAnswer() 
+    const checkedAnswer = checkAnswer(userAnswer, correctAnswer) 
+    const result =  getResult(checkedAnswer,userName,userAnswer,correctAnswer)  // Получаем результат (true/false) 
+    if (!result) {
+	return}
   }
-  console.log(`Congratulations, ${userName}!`)
+
+console.log(`Congratulations, ${userName}!`)  
 }
 export { brainEven }
